cedbc3f24a9c1fddb03cf7f6cba11f65bceb8685
Steve <thesteveoninja@gmail.com>
2016-11-12 12:57:41 -0500

    Animation Started

--- GJ2016 - Train Robbing Sim/Assets/Scripts/PlayerCharacter_1.cs
+++ GJ2016 - Train Robbing Sim/Assets/Scripts/PlayerCharacter_1.cs
@@ -5,6 +5,7 @@ public class PlayerCharacter_1 : MonoBehaviour
     public GameObject Player;
     public int Health;
     public int Money;
+    private Animator animator;
     
 
     // Use this for initialization
@@ -21,6 +22,26 @@ public class PlayerCharacter_1 : MonoBehaviour
         if (Health <= 0)
         {
             Destroy(gameObject);
+        }
+
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+
+        if (vertical > 0)
+        {
+            animator.SetInteger("Direction", 2);
+        }
+        else if (vertical < 0)
+        {
+            animator.SetInteger("Direction", 0);
+        }
+        else if (horizontal > 0)
+        {
+            animator.SetInteger("Direction", 1);
+        }
+        else if (horizontal < 0)
+        {
+            animator.SetInteger("Direction", 3);
         }
     }
 
