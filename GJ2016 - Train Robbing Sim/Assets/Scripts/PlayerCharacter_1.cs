eefeb39f102b8f73637595a27af5a43ae7452b06
Steve <thesteveoninja@gmail.com>
2016-11-11 20:01:16 -0500

    Added Health and Money w/ Initialization

---
+++ GJ2016 - Train Robbing Sim/Assets/Scripts/PlayerCharacter_1.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter_1 : MonoBehaviour {
+
+    public int Health;
+    public int Money;
+
+
+	// Use this for initialization
+	void Start () {
+
+        Health = 100;
+        Money = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
