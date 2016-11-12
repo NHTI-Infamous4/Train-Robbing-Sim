3bccf18d6db344e95065fb118f291bb6f80fe74e
Steve <thesteveoninja@gmail.com>
2016-11-12 10:43:12 -0500

    Main Menu, Level Music

---
+++ GJ2016 - Train Robbing Sim/Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class MenuMusic : MonoBehaviour {
+
+    public AudioMixerSnapshot Level_1;
+    public AudioClip stings;
+    public AudioSource stingSource;
+
+	// Use this for initialization
+	void Start () {
+        stingSource.clip = stings;
+        stingSource.Play();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        
+	}
+}
