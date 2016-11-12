679b91d4faeb24d89e8d50c199c5bdfc3ca7a549
Steve <thesteveoninja@gmail.com>
2016-11-12 14:59:34 -0500

    Added LevelMusic script

---
+++ GJ2016 - Train Robbing Sim/Assets/Scripts/LevelMusic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class LevelMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        public AudioMixerSnapshot Normal;
+
+
+}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
