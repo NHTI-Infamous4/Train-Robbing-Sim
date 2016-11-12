c6adc0576adb5d5b035cb0794c9d9032830a530f
Steve <thesteveoninja@gmail.com>
2016-11-12 15:12:06 -0500

    Level Music Code Added

--- GJ2016 - Train Robbing Sim/Assets/Scripts/LevelMusic.cs
+++ GJ2016 - Train Robbing Sim/Assets/Scripts/LevelMusic.cs
@@ -4,15 +4,34 @@ using UnityEngine.Audio;
 
 public class LevelMusic : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        public AudioMixerSnapshot Normal;
+    public AudioMixerSnapshot Level;
+    public AudioClip Normal;
+    public AudioClip Intense;
+    public AudioSource Source;
 
 
-}
+    // Use this for initialization
+    void Start() {
+
+        Source.clip = Normal;
+        Source.Play();
+    }
+
+
 	
 	// Update is called once per frame
 	void Update () {
 	
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Source.clip = Intense;
+            Source.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Source.clip = Normal;
+            Source.Play();
+        }
 	}
 }
